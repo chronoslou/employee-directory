3c9775fe6e042ede340d9d8464381786b054d3bf
Lou Falla <lou3892@gmail.com>
2021-06-08 20:40:29 -0400

    fixed readme

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -3,7 +3,7 @@ const Nav = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Navbar
+          Employee Directory
         </a>
         <button
           className="navbar-toggler"
